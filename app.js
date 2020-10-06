e64ca74d4b83414e6b39c772dfa5454bd9ea63f4
Chaitanya <chaitu.vsh@gmail.com>
2020-10-06 22:58:53 +0530

    Created an end-point with Express Framework

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 var http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer(function (request, response) {
-  if (request.url === "/") {
-    response.write("In the HomeScreen");
-    response.end();
-  }
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
 
-  if (request.url === "/api/courses") {
-    response.write(JSON.stringify(["CSE", "IT", "ECE", "MECH", "CIVIL"]));
-    response.end();
-  }
+app.get("/", (req, res) => {
+  /**
+   * http://expressjs.com/en/4x/api.html#req
+   *
+   * Documentation for the Express requests
+   */
+
+  res.send("Hello Maverick");
 });
 
-server.listen(7000);
+app.get("/api/courses", (req, res) => {
+  res.send(JSON.stringify(["CSE", "IT", "ECE", "MECH", "CIVIL", "BIO"]));
+});
 
-console.log("App Listening on 7000");
+app.listen(4000, () => {
+  console.log("Listening ra rey baabu");
+});
+
+// const server = http.createServer(function (request, response) {
+//   if (request.url === "/") {
+//     response.write("In the HomeScreen");
+//     response.end();
+//   }
+
+//   if (request.url === "/api/courses") {
+//     response.write(
+//       JSON.stringify(["CSE", "IT", "ECE", "MECH", "CIVIL", "BIO"])
+//     );
+//     response.end();
+//   }
+// });
+
+// server.listen(7000);
+
+// console.log("App Listening on 7000");
